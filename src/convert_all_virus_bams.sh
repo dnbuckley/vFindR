3068d5471b9c5258cdbd93aabe40c7c38b9f5680
dnbuckley <dnbuckle@usc.edu>
2021-08-03 16:27:29 -0700

    updata

--- src/convert_all_virus_bams.sh
+++ src/convert_all_virus_bams.sh
@@ -6,8 +6,7 @@ BAMTOOLS=$2
 cd $DIR
 BAMS=`ls -d *bam`
 
-for (( i=0; i<${#BAMS[@]}; i++ )); do
-	BAM=${BAMS[$i]}
+for BAM in $BAMS; do
 	BED=`echo $BAM | sed 's/bam$/bed/g'`
 	CMD="$BAMTOOLS convert -format bed -in $BAM -out $BED"
 	echo $CMD
